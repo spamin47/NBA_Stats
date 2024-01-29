2abc113a04415af4f20da05974f1a10b0c842a1c
Minh Nguyen <mnguyen4725@gmail.com>
2024-01-29 03:15:19 -0800

    clean up

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-osterPageExtension = doc.find(string=" Roster ").parent['href']
-        # rosterWebpage = requests.get(mainPageUrl+rosterPageExtension)
-        # doc = BeautifulSoup(rosterWebpage.text,"html.parser")
-        # playerPosition = doc.find(string=f" {playerName} ").parent.parent.parent.parent.find_all("td")[2].find("span").string
